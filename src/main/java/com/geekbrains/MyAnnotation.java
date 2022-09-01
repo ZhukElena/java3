774c08fb160d7eb34ea53e4b14f9def645a8d0ad
lenazhuk <vaitler87@icloud.com>
2022-09-01 20:46:03 +0300

    hw7

---
+++ src/main/java/com/geekbrains/MyAnnotation.java
@@ -0,0 +1,12 @@
+package com.geekbrains;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface MyAnnotation {
+    int priority() default 1;
+}
